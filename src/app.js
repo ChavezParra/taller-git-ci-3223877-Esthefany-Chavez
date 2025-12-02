77863e29eb24818ab23b96ddf218f5624e79b59e
Esthefany Chavez <chavez.esthefany07@gmail.com>
2025-12-02 10:19:27 -0500

    feat: agregar función agregarItem para UI

---
+++ src/app.js
@@ -0,0 +1,1 @@
+function suma(a,b){return a+b;}function agregarItem(texto){const li=document.createElement('li');li.textContent=texto;document.getElementById('lista').appendChild(li);}console.log('App lista');
