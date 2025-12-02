aebfafa93deb30ad86d05f0c087ac80ef50a4924
Esthefany Chavez <chavez.esthefany07@gmail.com>
2025-12-02 10:25:19 -0500

    chore: cambio mínimo para abrir PR

--- src/app.js
+++ src/app.js
@@ -1,1 +1,2 @@
 function suma(a,b){return a+b;}function agregarItem(texto){const li=document.createElement('li');li.textContent=texto;document.getElementById('lista').appendChild(li);}console.log('App lista');
+// cambio mínimo para PR
